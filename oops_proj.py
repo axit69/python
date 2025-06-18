3456b2da710dcfd16f94ea1e4678bd98c0d6b68c
axitmehta <axitmehta6@gmail.com>
2025-06-18 22:29:44 +0530

    features added till post

---
+++ oops_proj.py
@@ -0,0 +1,65 @@
+class chatbook:
+    def __init__(self):
+        self.username = ''
+        self.pwd = ''
+        self.loggedin = False
+        self.menu()
+
+    def menu(self):
+        user_input = input("""Welcome to the chatbook!! 
+                            1. Press 1 to signup. 
+                            2. Press 2 to signin. 
+                            3. press 3 to write a post.
+                            4. press 4 to message a friend.
+                            5. press 5 to exit.""")
+        if user_input == "1":
+            self.signup()
+        elif user_input == "2":
+            self.signin()
+        elif user_input == "3":
+            self.post()
+        elif user_input == "4":
+            pass
+        else:
+            exit
+
+    def signup(self):
+        email = input("enter your email")
+        pwd = input("enter your paswword")
+        self.username = email
+        self.pwd = pwd
+        print("you have signeed up successfully!!")
+        print("\n")
+        self.menu()
+
+    def signin(self):
+        if self.username== '' and self.pwd== '':
+            print("first singup then signin: ")
+        else:
+            email = input("enter username ")
+            pwd = input("enter password ")
+            if self.username == email and self.pwd == pwd:
+                print("you have successfully signin !!")
+                self.loggedin = True
+                self.post()
+            else:
+                print("please provide correct username and password: ")
+                self.signin()
+        
+    def post(self):
+         email = input("enter username ")
+         pwd = input("enter password ")
+         if self.username == email and self.pwd == pwd:
+                print("you have successfully signin !!")
+                self.loggedin = True
+                postt = input("write a post: ")
+                print("you posted: " , postt)
+         else:
+                print("please signup to post ")
+                self.menu()
+        
+        
+
+
+obj = chatbook()
+        
